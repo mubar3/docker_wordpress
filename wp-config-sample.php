33a3a3a5570d9dec6d9404503928ea620870fb5a
mubar3 <khusnul.mubar@yahoo.com>
2024-01-20 08:29:26 +0700

    fix ftp

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -37,6 +37,8 @@ define( 'DB_CHARSET', 'utf8' );
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+// ketika eror ftp
+define('FS_METHOD', 'direct');
 /**#@+
  * Authentication unique keys and salts.
  *
